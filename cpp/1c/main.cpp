1bebec0e2b2d619164a5d76e5edc3d7355c3a7b6
Gunnar Thorsen Liahjell <gunnar@Abel-2.0>
2018-09-03 18:32:34 +0200

    supert

--- cpp/1c/main.cpp
+++ cpp/1c/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <time.h>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    int n = 100;
+    int n = 10;
     double h = 1./(n+1);
 
-    double *a,*b,*c,*d,*x,*f,*u;
-    a = new double[n];
+    double *b,*d,*x,*f,*u;
     b = new double[n];
-    c = new double[n];
     d = new double[n+2];
     x = new double[n+2];
     f = new double[n+2];
     u = new double[n+2];
+    double a = -1;
+    double c = -1;
+
+    double start, stop;
 
     for (int i = 0; i<n+2;i++){
         x[i] = i*h;
@@ -29,38 +32,34 @@ int main(int argc, char *argv[])
     d[0]  = 0;
     d[n+1] = 0;
     for (int i = 0; i < n; i++){
-        a[i] = -1;
         b[i] = 2;
-        c[i] = -1;
     }
 
-    a[0]  = 0;
-    c[-1] = 0;
     double cof;
 
-
+    start = clock();
     for (int i=1;i<n;i++){
-        cof     = a[i]/b[i-1];
-        b[i]   -= cof*c[i-1];
+        cof     = a/b[i-1];
+        b[i]   += cof;
         d[i+1] -= cof*d[i];
     }
 
 
     for (int i = n-1; i>0;i--){
-        d[i] -= (c[i-1]/b[i])*d[i+1];
+        d[i] += d[i+1]/b[i];
     }
 
-
+    stop = clock();
     for (int i = 0; i<n;i++){
         d[i+1] = d[i+1]/b[i];
     }
 
+    printf("%f\n", (stop-start)/CLOCKS_PER_SEC);
     ofstream outfile;
     outfile.open("outfile.txt");//argv[1])
     for (int i = 0; i<n+2;i++){
         outfile<< x[i]<<" "<<u[i]<<" "<< d[i]<<"\n";
     }
     outfile.close();
-
     return 0;
 }
