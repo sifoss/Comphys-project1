80fbb96afadeafc445fa30a7ce50a3f1965204a6
Gunnar <gunnartl@uio.no>
2018-09-07 22:25:27 +0200

    neeeeeeeeesten ferri

--- cpp/project1/main.cpp
+++ cpp/project1/main.cpp
@@ -14,25 +14,29 @@ void errors(int max_n);
 void compare(int max_n);
 
 int main(){
-    int n = 1000;
-    vec x,a,b,c,d;
+    int n = 10;
+    vec x,a,b,c,d,exact;
     double h = 1./(n+1);
     x = linspace(0,1,n+2);
     a.ones(n); a *= -1;   // diagonals in the dridiagonal matrix A. a=lower diagonal, c=upper b=middle
     b.ones(n); b *= 2;
     c.ones(n); c *= -1;
     d = h*h*f(x);
+    exact = analytical(x);
     d[0] = 0; d[n+1] = 0; a[0] = 0; c[n-1] = 0; // randbetingelser
 
     vec solution = general_elim_solve(a,b,c,d, n);     //1b
+    ofstream outfile;
+    outfile.open("u_exact_approx.txt");//argv[1])
+    for (int i = 0; i<n+2;i++){
+        outfile<< x[i]<<" "<<exact[i]<<" "<< solution[i]<<"\n";
+    }
+    outfile.close();
     vec fast_solution = specialized_elim_solve(b,d,n,1); //c
     cout<<endl;
     //errors(int(1e7)); //d output to file
-    compare(1000);      //e output to file
+    compare(10);      //e output to file
 
-
-    //cout << solution << endl;
-    //cout << fast_solution <<endl;
     return 0;
 }
 
@@ -94,8 +98,10 @@ vec specialized_elim_solve(vec b,vec b_vec,int n,int s){
 }
 
 void errors(int max_n){
-    vec err;
-    err.zeros(int(log10(max_n)));
+    vec err,n_vec;
+    int m = int(log10(max_n));
+    err.zeros(m);
+    n_vec.zeros(m);
     for (int n = 10;n<=max_n;n = n*10){
         vec x,b,d,exact,approx,eps;
         x = linspace(0,1,n+2);
@@ -111,15 +117,27 @@ void errors(int max_n){
         eps[0] = -200; // unngår randbetingelsene
         eps[n+1] = -200;
         err[int(log10(n))-1] = eps.max();
-
+        n_vec[int(log10(n))-1] = n;
     }
-    //cout << err << endl;
+    ofstream outfile;
+    outfile.open("errors.txt");//argv[1])
+    for (int i = 0; i<m;i++){
+        outfile<< n_vec[i]<<" "<<err[i]<<" "<<"\n";
+    }
+    outfile.close();
+
 }
 
 void compare(int max_n){
     //Comparing LU decomposition with the specialiced gaussian
     for(int n = 10; n<=max_n; n = n*10){
-
+        vec x,b,d,gaus_solution;
+        x = linspace(0,1,n+2);
+        d = (1./(n+1))*(1./(n+1))*f(x);
+        vec d_for_matrix(n);
+        for(int i = 0; i<n;i++){
+            d_for_matrix[i] = d[i+1];
+        }
         //LU
         mat A(n,n);
         mat L,U;
@@ -135,14 +153,13 @@ void compare(int max_n){
         start = clock();
         lu(L,U,A);
         stop = clock();
-        vec arma_solution;
+        vec y = solve(L,d_for_matrix);
+        vec arma_solution = solve(U,y);
+        cout<<arma_solution;
         printf("Armadillos LU decomp %f secs with n= %i \n", (stop-start)/CLOCKS_PER_SEC,n);
 
         //Gauss
-        vec x,b,d,gaus_solution;
-        x = linspace(0,1,n+2);
         b.ones(n); b *= 2;
-        d = (1./(n+1))*(1./(n+1))*f(x);
         gaus_solution = specialized_elim_solve(b,d,n,1);
         cout<<endl;
 
